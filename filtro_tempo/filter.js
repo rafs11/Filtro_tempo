87724a61dc0f9462a101f953014ad3311358c77c
Rafael Santana <57414652+rafs11@users.noreply.github.com>
2022-08-11 13:28:55 -0300

    atualização do código

--- filtro_tempo/filter.js
+++ filtro_tempo/filter.js
@@ -1,53 +1,45 @@
-//ticket de exemplo
-const tickets = {
-  id: 'código de id',
-  ticket: 'código do ticket',
-  status: 'status',
-  service_name: 'nome do serviço',
-  serice_asset: 'ativo de serviço', //até o momento essas informações são restritas e não importam para o desenvolvimento desse código
-  open_date: '28/07/2022 11:00:36',
-  resolve_date: null,
-  prev_retorno: '29/07/2022 03:00:36'
-}
+let value = '{"data":{"tickets":[{"id":"numeroDeID","ticket":"numeroDeTicket","status":"st","service_name":"Nome do serviço","service_asset":"asst","open_date":"11/08/2022 07:24:46","resolve_date":null,"prev_retorno":"11/08/2022 23:24:46"}]},"mensagem":"OK"}'
 
-// valor de abertura do date
-open_date = tickets.open_date
+// algumas informações desse ticket foi ocultado por segurança. As informações necessário são somente as datas
 
-// colocando no formato US
-day = open_date.toString().substring(0, 2)
-month = open_date.toString().substring(3, 5)
-year = open_date.toString().substring(6, 10)
-hour = open_date.toString().substring(11, 19)
+const json = JSON.parse(value)
+const ticket = json.data.tickets
 
-formatUs = year + '/' + month + '/' + day + ' ' + hour
+open_date = ticket[0].open_date
 
-var t2 = new Date(formatUs)
+// separado para montar a data no formato US
+dia = open_date.toString().substring(0,2)
+mes = open_date.toString().substring(3,5)
+ano = open_date.toString().substring(6,10)
+hora = open_date.toString().substring(open_date.length - 8)
 
-// valor de fechamento do date
+// aqui junta no formato US
+formatUs = ano + '-' + mes + '-' + dia + ' ' + hora
 
-if (
-  typeof tickets.resolve_date === 'undefined' ||
-  tickets.resolve_date === null
-) {
-  close_date = tickets.prev_retorno
+const t2 = new Date(formatUs)
+
+
+// condicional para filtrar resolve_date ou prev_retorno
+if (!ticket[0].resolve_date){
+  close_date = ticket[0].prev_retorno
 } else {
-  close_date = tickets.resolve_date
+  close_date = ticket[0].resolve_date
 }
 
-// colocando no formato US
-day = close_date.toString().substring(0, 2)
-month = close_date.toString().substring(3, 5)
-year = close_date.toString().substring(6, 10)
-hour = close_date.toString().substring(11, 19)
+// separado para montar a data no formato US
+dia = close_date.toString().substring(0,2)
+mes = close_date.toString().substring(3,5)
+ano = close_date.toString().substring(6,10)
+hora = close_date.toString().substring(close_date.length - 8)
 
-formatUs = year + '/' + month + '/' + day + ' ' + hour
+// aqui junta no formato US
+formatUs = ano + '-' + mes + '-' + dia + ' ' + hora
 
-var t1 = new Date(formatUs)
+// results
+const t1 = new Date(formatUs) // datatime atual
 
-// resultado do time
+const diffMS = (t1.getTime() - t2.getTime())/(60*1000) // milissegundos para minutos
 
-let diffMS = (t1.getTime() - t2.getTime()) / 60000
+// return diffMS //zabbix
 
-// return diffMS // zabbix
-
-console.log(diffMS + 'min') // teste de funcionamento
+console.log(diffMS)
